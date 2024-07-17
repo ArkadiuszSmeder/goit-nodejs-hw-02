0f48fa88bafb1123118b8a56554d7f6e2c4a4368
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-07-17 14:06:21 +0200

    Add Joi for user

--- controlers/usersCtrl.js
+++ controlers/usersCtrl.js
@@ -1,6 +1,13 @@
 const User = require('../models/users.js');
 const Joi = require('joi');
 
+const userJoiSchema = Joi.object({
+    password: Joi.string().min(8).max(20).required(),
+    email: Joi.string().email().required(),
+    subscription: Joi.string().valid('starter', 'pro', 'business').default('starter'),
+    token: Joi.string().allow(null)
+})
+
 const createUser = (req, res, next) => {
 
 };

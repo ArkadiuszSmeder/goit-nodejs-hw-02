0af15e4ebeb2ed3f87331e319204f61f8149382c
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-07-17 11:46:39 +0200

    Add owner key in contactSchema

--- models/contacts.js
+++ models/contacts.js
@@ -16,7 +16,11 @@ const contactSchema = new mongoose.Schema({
   favorite: {
     type: Boolean,
     default: false,
-},
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  }
 },
   {
     versionKey: false

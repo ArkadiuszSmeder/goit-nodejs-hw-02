89d5314fcbd841d5d23c3b5fda9b0165f11801f5
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-07-17 12:38:50 +0200

    Create usersRouter

--- routes/api/api.js
+++ routes/api/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const contactsRoute = require('./contacts.js');
+const usersRoute = require('./users.js');
 
-router.use('/contacts', contactsRoute)
+router.use('/contacts', contactsRoute);
+router.use('/users', usersRoute);
 
 module.exports = router;

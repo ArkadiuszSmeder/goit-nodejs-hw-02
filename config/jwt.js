ad462e91b7bbfdff97f63824ad92f8f7fbfe5a4f
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-07-19 08:31:23 +0200

    Add JWTStrategy in config folder

--- config/jwt.js
+++ config/jwt.js
@@ -1,1 +1,30 @@
 const passport = require('passport');
+const { ExtractJwt, Strategy: JWTStrategy } = require('passport-jwt');
+const User = require('../models/users.js');
+
+export default function setJWTStrategy() {
+    const secret = process.env.SECRET;
+
+    const params = {
+        secretOrKey: secret,
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    }
+
+    passport.use(
+        new JWTStrategy(
+            params,
+            async function (payload, done) {
+                try {
+                    const user = await User.findOne({_id: payload.id}).lean();
+
+                    if (!user) {
+                        return done(new Error('User not found'))
+                    }
+                    return done(null, user)
+                } catch (err) {
+                    return done(err)
+                }
+            }
+        )
+    )
+}

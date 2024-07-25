c60ef8743f4965d80c2790dc765b59d1bc350ca8
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-07-25 12:01:13 +0200

    Add diskStorage

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.use(express.static(path.resolve(__dirname, "./public")));
 const tempDir = path.join(process.cwd(), "tmp");
 const storeImageDir = path.join(process.cwd(), "public/avatars");
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => { 
+      cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+      cb(null, `${uuidV4()}${file.originalname}`);
+  },
+});
 
 const { DB_URL: urlDB } = process.env;
 

b48e25668c9e7697931bc55e1143d521c86fa29b
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-07-25 13:25:16 +0200

    Add isImageAndTransform func
    Needed for transform avatar

---
+++ config/avatarSet.js
@@ -0,0 +1,25 @@
+const Jimp = require("jimp");
+
+const MAX_AVATAR_SIZE = 250;
+
+const isImageAndTransform = async (path) =>
+    new Promise((resolve) => {
+        Jimp.read(path, async (err, image) => {
+            if (err) resolve(false)
+            
+            try {
+                await image
+                    .rotate(360)
+                    .resize(MAX_AVATAR_SIZE, MAX_AVATAR_SIZE)
+                    .write(path)
+                resolve(true)
+            } catch (err) {
+                console.log(err)
+                resolve(false)
+            }
+        })
+    })
+
+module.exports = {
+    isImageAndTransform
+}

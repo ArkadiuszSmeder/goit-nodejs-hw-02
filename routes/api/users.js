f552360e1860849ea7c542af4a8f425867a3b5a2
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-07-25 09:54:04 +0200

    Add /avatars route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, loginUser, logoutUser, getCurrentUser } = require('../../controlers/usersCtrl.js');
+const { createUser, loginUser, logoutUser, getCurrentUser, updateAvatar } = require('../../controlers/usersCtrl.js');
 const authMiddleware = require('../../middleware/jwt.js');
 
 router.post('/signup', createUser);
@@ -10,5 +10,7 @@ router.post('/login', loginUser);
 router.get('/logout', authMiddleware, logoutUser);
 
 router.get('/current', authMiddleware, getCurrentUser)
+
+router.patch('/avatars', authMiddleware, updateAvatar);
 
 module.exports = router;

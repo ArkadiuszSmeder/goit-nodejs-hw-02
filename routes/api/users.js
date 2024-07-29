a91d245ef141fd9702c4c21ca864174a136e98a2
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-07-29 07:54:07 +0200

    Create verify endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, loginUser, logoutUser, getCurrentUser, updateAvatar } = require('../../controlers/usersCtrl.js');
+const { createUser, loginUser, logoutUser, getCurrentUser, updateAvatar, verifyUser } = require('../../controlers/usersCtrl.js');
 const authMiddleware = require('../../middleware/jwt.js');
 const multerMiddleware = require('../../config/multer.js');
 
@@ -13,5 +13,7 @@ router.get('/logout', authMiddleware, logoutUser);
 router.get('/current', authMiddleware, getCurrentUser)
 
 router.patch('/avatars', authMiddleware, multerMiddleware.single('avatar'), updateAvatar);
+
+router.get('/verify/:verificationToken', verifyUser);
 
 module.exports = router;
